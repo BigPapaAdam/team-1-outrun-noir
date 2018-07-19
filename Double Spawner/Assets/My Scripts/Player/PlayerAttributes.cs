84bcac8034e215f2dc7791a5a3d05beed22d1d38
JesseSmart <jesse.oscar.smart@gmail.com>
2018-07-19 21:49:04 +1000

    Created a environment generator, and made a script which controls player health, allied mafia, and pick up timers. This script is in progress.

---
+++ Double Spawner/Assets/My Scripts/Player/PlayerAttributes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerAttributes : MonoBehaviour {
+
+    public float playerMaxHealth;
+    public float playerHealth;
+    public Image healthBarUI;
+
+    public int activeAllies;
+    public Image allyIcon1UI;
+    public Image allyIcon2UI;
+    public Image allyIcon3UI;
+
+    public enum activeWeapon { Pistol, Tommy, Shotgun }
+    public float pickupTimer;
+    public float pickupDuration;
+    public Text weaponTimerUI;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        healthBarUI.fillAmount = playerHealth / playerMaxHealth;
+	}
+}
