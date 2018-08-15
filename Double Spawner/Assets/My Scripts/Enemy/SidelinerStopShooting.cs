f20418c4fa63ec4431e5db9e1e9676afc9ec44fe
Gords1229 <40745592+Gords1229@users.noreply.github.com>
2018-08-15 20:45:08 +1000

    fixed the mafia pick up and triad car animations, with some blance changes

---
+++ Double Spawner/Assets/My Scripts/Enemy/SidelinerStopShooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SidelinerStopShooting : MonoBehaviour {
+
+    public GameObject bulletSpawn1;
+    public GameObject bulletSpawn2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider collide)
+    {
+        if (collide.gameObject.tag == "MotoActivator")
+        {            
+            bulletSpawn1.GetComponent<EnemyShooter>().enabled = false;
+            bulletSpawn2.GetComponent<EnemyShooter>().enabled = false;
+        }
+    }
+}
